7445e6588c70c377cc42d42e8508426347e5dfc7
Jesus Lopez <jlmwsk32@gmail.com>
2024-02-19 13:24:45 -0600

    c1: boton saludo con nombre

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hola Ch38")
+// Funcion que da un saludo con alert
+function saludo() {
+    alert('¡Hola Ch38!');
+}
+/**
+ * Pedir el nombre de la persona usando un Promt ()
+ * Mostrar un saludo y nombre de la persona con alert()
+ */
+function enviarSaludoAPersona() {
+    let message = prompt("Hola usuario dime tu nombre");
+    alert(`Hola ${message}`);
+}
